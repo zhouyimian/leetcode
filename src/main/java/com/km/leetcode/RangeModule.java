52a082441497c64115b486fe92f8a7622f36c01c
km <313976009@qq.com>
2018-12-09 09:46:18 +0800

    715. Range Module

---
+++ src/main/java/com/km/leetcode/RangeModule.java
@@ -0,0 +1,49 @@
+package com.km.leetcode;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class RangeModule {
+    TreeMap<Integer,Integer> map=new TreeMap();
+    public RangeModule() {
+    }
+
+    public void addRange(int left, int right) {
+        if(left>=right)
+            return;
+        Integer start=map.floorKey(left);
+        Integer end=map.floorKey(right);
+        if(start==null&&end==null)
+            map.put(left,right);
+        else if(start!=null&&map.get(start)>=left)
+            map.put(start,Math.max(map.get(end),Math.max(map.get(start),right)));
+        else
+            map.put(left,Math.max(right,map.get(end)));
+        Map<Integer,Integer> subMap = map.subMap(left,false,right,true);
+        Set<Integer> set = new HashSet(subMap.keySet());
+        map.keySet().removeAll(set);
+    }
+
+    public boolean queryRange(int left, int right) {
+        Integer start=map.floorKey(left);
+        if(start==null)
+            return false;
+        return map.get(start)>=right;
+    }
+
+    public void removeRange(int left, int right) {
+        if(right<=left)
+            return;
+        Integer start=map.floorKey(left);
+        Integer end = map.floorKey(right);
+        if(end!=null&&map.get(end)>right)
+            map.put(right,map.get(end));
+        if(start!=null&&map.get(start)>left)
+            map.put(start,left);
+        Map<Integer,Integer> subMap = map.subMap(left,true,right,false);
+        Set<Integer> set = new HashSet(subMap.keySet());
+        map.keySet().removeAll(set);
+    }
+}
