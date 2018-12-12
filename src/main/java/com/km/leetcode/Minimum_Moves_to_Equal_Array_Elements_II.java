a5acf43050eda7e2aa4be41cc182d72791b9a8c7
km <313976009@qq.com>
2018-12-12 10:09:46 +0800

    462. Minimum Moves to Equal Array Elements II

--- src/main/java/com/km/leetcode/Minimum_Moves_to_Equal_Array_Elements_II.java
+++ src/main/java/com/km/leetcode/Minimum_Moves_to_Equal_Array_Elements_II.java
@@ -2,20 +2,15 @@ package com.km.leetcode;
 
 import sun.nio.cs.ext.MacThai;
 
+import java.util.Arrays;
+
 public class Minimum_Moves_to_Equal_Array_Elements_II {
     public int minMoves2(int[] nums) {
-        int ans=Integer.MAX_VALUE,min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
-        int sum=0;
-        for(int num:nums){
-            sum+=num;
-            if(num>max)
-                max=num;
-            if(num<min)
-                min=num;
-        }
-        for(int i=min;i<=max;i++){
-            ans= Math.min(ans,Math.abs(nums.length*i-sum));
-        }
+        int ans=0;
+        Arrays.sort(nums);
+        int i=0,j=nums.length-1;
+        while(i<j)
+            ans+=(nums[j--]-nums[i++]);
         return ans;
     }
 }
