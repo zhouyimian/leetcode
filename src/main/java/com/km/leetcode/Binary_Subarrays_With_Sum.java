80f821483c9edee92c2aa8432a38ca558d5dcc5d
km <313976009@qq.com>
2018-12-21 16:55:27 +0800

    424. Longest Repeating Character Replacement

--- src/main/java/com/km/leetcode/Binary_Subarrays_With_Sum.java
+++ src/main/java/com/km/leetcode/Binary_Subarrays_With_Sum.java
@@ -2,45 +2,14 @@ package com.km.leetcode;
 
 public class Binary_Subarrays_With_Sum {
 	public int numSubarraysWithSum(int[] A, int S) {
-		int ans = 0;
-		int[] counts = new int[A.length];
-		int[] next_1 = new int[A.length];
-		next_1[A.length-1]=-1;
-		counts[0] = A[0] == 0 ? 0 : 1;
-		for (int i = 1; i < A.length; i++)
-			counts[i] = A[i] == 0 ? counts[i - 1] : counts[i - 1] + 1;
-		for(int i=A.length-2;i>=0;i--)
-			next_1[i]=A[i+1]==1?i+1:next_1[i+1];
-		int first = 0, second = 0;
-		while (first <= second && second < A.length) {
-			if (S == 0) {
-				while (first < A.length && A[first] != 0)
-					first++;
-				second = first + 1;
-				while (second < A.length && A[second] == 0)
-					second++;
-				second--;
-				if (first == second)
-					ans++;
-				else
-					ans += Math.pow(2, (second - first)) - 1;
-				first = second + 1;
-				second = first + 1;
-			} else {
-				if (counts[second] - counts[first] + A[first] == S) {
-					int len_second=next_1[second]-second;
-					int len_first=next_1[first]-first;
-					if(A[first]==0&&A[second]==0) {
-						
-					}
-					else {
-						
-					}
-				} else if (counts[second] - counts[first] + A[first] < S)
-					second++;
-				else
-					first++;
-			}
+		int ans=0,count[]=new int[A.length+1];
+		count[0]=1;
+		int psum=0;
+		for(int num:A){
+			psum+=num;
+			if(psum>=S)
+				ans+=count[psum-S];
+			count[psum]++;
 		}
 		return ans;
 	}
--- src/main/java/com/km/leetcode/Maximum_Depth_of_N_ary_Tree.java
+++ src/main/java/com/km/leetcode/Maximum_Depth_of_N_ary_Tree.java
@@ -3,19 +3,19 @@ package com.km.leetcode;
 import java.util.List;
 
 
-class Node {
-    public int val;
-    public List<com.km.leetcode.Node> children;
-
-    public Node() {
-    }
-
-    public Node(int _val, List<com.km.leetcode.Node> _children) {
-        val = _val;
-        children = _children;
-    }
-}
 public class Maximum_Depth_of_N_ary_Tree {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
     public int maxDepth(Node root) {
         if (root == null)
             return 0;
