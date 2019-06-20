2b0c30a2a340f1507863c1a47b7fe792a19e4618
zhouyimian <313976009@qq.com>
2019-06-20 20:27:21 +0800

    test

--- src/main/java/com/km/App.java
+++ src/main/java/com/km/App.java
@@ -1,20 +1,12 @@
 package com.km;
 
-import java.util.*;
-
 /**
  * Hello world!
  */
 public class App {
+
     public static void main(String[] args) {
-        System.out.println(Base2(-1));
-    }
-    public static String Base2(int num){
-        StringBuilder sb = new StringBuilder();
-        while(num!=0){
-            sb.append(num&1);
-            num = num>>1;
-        }
-        return sb.reverse().toString();
+        System.out.println("hello world");
+        System.out.println("test");
     }
 }
