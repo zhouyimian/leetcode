bcde2ccc942a65bd85df81a6ec6ee0f020017a59
zhouyimian <313976009@qq.com>
2019-06-21 16:29:57 +0800

    WeeklyContest141

--- src/main/java/com/km/WeeklyContest141/Shortest_Common_Supersequence.java
+++ src/main/java/com/km/WeeklyContest141/Shortest_Common_Supersequence.java
@@ -3,19 +3,13 @@ package com.km.WeeklyContest141;
 public class Shortest_Common_Supersequence {
     public String shortestCommonSupersequence(String str1, String str2) {
         int[][] dp = new int[str1.length()+1][str2.length()+1];
-        for(int i = 0;i<=str1.length();i++){
-            dp[i][0] = i;
-        }
-        for(int i = 0;i<=str2.length();i++){
-            dp[0][i] = i;
-        }
         for(int i = 1;i<=str1.length();i++){
             for(int j = 1;j<=str2.length();j++){
                 if(str1.charAt(i-1)==str2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1]+1;
                 }
                 else{
-                    dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1])+1;
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
@@ -35,7 +29,7 @@ public class Shortest_Common_Supersequence {
                 sb.append(str1.charAt(index1-1));
                 index1--;
                 index2--;
-            }else if(dp[index1-1][index2]<dp[index1][index2-1]){
+            }else if(dp[index1][index2]==dp[index1-1][index2]){
                 sb.append(str1.charAt(index1-1));
                 index1--;
             }else{
